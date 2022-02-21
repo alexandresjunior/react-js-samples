ade4ff176e07a799bbd073f2cdd576748e954159
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2022-02-21 16:18:40 -0300

    Use multi step nav component

--- user-registration-form/src/components/ContactData.js
+++ user-registration-form/src/components/ContactData.js
@@ -7,7 +7,7 @@ import { search } from "../api/index";
 // Imports the @clayui/css package CSS
 import "@clayui/css/lib/css/atlas.css";
 
-const ContactData = () => {
+const ContactData = ({currentStep, setCurrentStep}) => {
 
     const [homePhone, setHomePhone] = useState([]);
     const [mobilePhone, setMobilePhone] = useState([]);
@@ -29,150 +29,142 @@ const ContactData = () => {
     }
 
     return (
-        <div className="container col col-6">
-            <div className="text-center mt-3 mb-3">
-                <span>Step 2 of 3</span>
-            </div>
+        <ClayForm id="registration-contact-data" onSubmit={onSubmit}>
+            <ClayPanel displayTitle="Contact Data" displayType="unstyled">
+                <ClayPanel.Body>
+                    <ClayForm.Group>
+                        <label htmlFor="homePhone">Home Phone</label>
+                        <ClayInput 
+                            id="homePhone"
+                            name="homePhone"
+                            placeholder="Insert your home phone here"
+                            type="text"
+                            required
+                            onChange={(event) => {
+                                setHomePhone(event.target.value);
+                            }}
+                        />
+                    </ClayForm.Group>
 
-            <h3 className="text-center mb-3">
-                Let's register your account!
-            </h3>
+                    <ClayForm.Group>
+                        <label htmlFor="mobilePhone">Mobile Phone</label>
+                        <ClayInput 
+                            id="mobilePhone"
+                            name="mobilePhone"
+                            placeholder="Insert your mobile phone here"
+                            type="text"
+                            required
+                            onChange={(event) => {
+                                setMobilePhone(event.target.value);
+                            }}
+                        />
+                    </ClayForm.Group>
 
-            <ClayForm id="registration-contact-data" onSubmit={onSubmit}>
-                <ClayPanel displayTitle="Contact Data" displayType="unstyled">
-                    <ClayPanel.Body>
-                        <ClayForm.Group>
-                            <label htmlFor="homePhone">Home Phone</label>
-                            <ClayInput 
-                                id="homePhone"
-                                name="homePhone"
-                                placeholder="Insert your home phone here"
-                                type="text"
-                                required
-                                onChange={(event) => {
-                                    setHomePhone(event.target.value);
-                                }}
-                            />
-                        </ClayForm.Group>
+                    <ClayForm.Group>
+                        <label htmlFor="address1">Address 1</label>
+                        <ClayInput 
+                            id="address1"
+                            name="address1"
+                            placeholder="Insert your address here"
+                            type="text"
+                            required
+                            onChange={(event) => {
+                                setAddress1(event.target.value);
+                            }}
+                        />
+                    </ClayForm.Group>
 
-                        <ClayForm.Group>
-                            <label htmlFor="mobilePhone">Mobile Phone</label>
-                            <ClayInput 
-                                id="mobilePhone"
-                                name="mobilePhone"
-                                placeholder="Insert your mobile phone here"
-                                type="text"
-                                required
-                                onChange={(event) => {
-                                    setMobilePhone(event.target.value);
-                                }}
-                            />
-                        </ClayForm.Group>
+                    <ClayForm.Group>
+                        <label htmlFor="address2">Address 2</label>
+                        <ClayInput 
+                            id="address2"
+                            name="address2"
+                            placeholder="Complement your address here"
+                            type="text"
+                            required
+                            onChange={(event) => {
+                                setAddress2(event.target.value);
+                            }}
+                        />
+                    </ClayForm.Group>
 
-                        <ClayForm.Group>
-                            <label htmlFor="address1">Address 1</label>
-                            <ClayInput 
-                                id="address1"
-                                name="address1"
-                                placeholder="Insert your address here"
-                                type="text"
-                                required
-                                onChange={(event) => {
-                                    setAddress1(event.target.value);
-                                }}
-                            />
-                        </ClayForm.Group>
+                    <ClayForm.Group>
+                        <label htmlFor="city">City</label>
+                        <ClayInput 
+                            id="city"
+                            name="city"
+                            placeholder="Insert your city here"
+                            type="text"
+                            required
+                            onChange={(event) => {
+                                setCity(event.target.value);
+                            }}
+                        />
+                    </ClayForm.Group>
 
-                        <ClayForm.Group>
-                            <label htmlFor="address2">Address 2</label>
-                            <ClayInput 
-                                id="address2"
-                                name="address2"
-                                placeholder="Complement your address here"
-                                type="text"
-                                required
-                                onChange={(event) => {
-                                    setAddress2(event.target.value);
-                                }}
-                            />
-                        </ClayForm.Group>
+                    <ClayForm.Group>
+                        <label htmlFor="state">State</label>
+                        <ClaySelect 
+                            id="state"
+                            name="state"
+                            defaultValue={state.id}
+                            onClick={(event) => {
+                                setState(event.target.value);                            
+                            }}
 
-                        <ClayForm.Group>
-                            <label htmlFor="city">City</label>
-                            <ClayInput 
-                                id="city"
-                                name="city"
-                                placeholder="Insert your city here"
-                                type="text"
-                                required
-                                onChange={(event) => {
-                                    setCity(event.target.value);
-                                }}
-                            />
-                        </ClayForm.Group>
+                        >
+                            {stateList.map((state) => (
+                                <ClaySelect.Option 
+                                    key={state.id}
+                                    label={state.name}
+                                    value={state.id}
+                                />
+                            ))}
+                        </ClaySelect>
+                    </ClayForm.Group>
 
-                        <ClayForm.Group>
-                            <label htmlFor="state">State</label>
-                            <ClaySelect 
-                                id="state"
-                                name="state"
-                                defaultValue={state.id}
-                                onClick={(event) => {
-                                    setState(event.target.value);                            
-                                }}
+                    <ClayForm.Group>
+                        <label htmlFor="zipCode">Zip Code</label>
+                        <ClayInput 
+                            id="zipCode"
+                            name="zipCode"
+                            placeholder="Insert your zip code here"
+                            type="number"
+                            required
+                            onChange={(event) => {
+                                setZipCode(event.target.value);
+                            }}
+                        />
+                    </ClayForm.Group>
+                </ClayPanel.Body>
+            </ClayPanel>
 
-                            >
-                                {stateList.map((state) => (
-                                    <ClaySelect.Option 
-                                        key={state.id}
-                                        label={state.name}
-                                        value={state.id}
-                                    />
-                                ))}
-                            </ClaySelect>
-                        </ClayForm.Group>
+            <div className="sheet-footer">
+                <ClayButton.Group>
+                    <div className="btn-group-item">
+                        <ClayButton
+                            displayType="secondary"
+                            onClick={() => setCurrentStep(currentStep - 1)}                   
+                        >
+                            Previous
+                        </ClayButton>
+                    </div>
 
-                        <ClayForm.Group>
-                            <label htmlFor="zipCode">Zip Code</label>
-                            <ClayInput 
-                                id="zipCode"
-                                name="zipCode"
-                                placeholder="Insert your zip code here"
-                                type="number"
-                                required
-                                onChange={(event) => {
-                                    setZipCode(event.target.value);
-                                }}
-                            />
-                        </ClayForm.Group>
-                    </ClayPanel.Body>
-                </ClayPanel>
-
-                <div className="sheet-footer">
-                    <ClayButton.Group>
-                        <div className="btn-group-item">
-                            <ClayButton
-                                onClick={() => {
-                                    // TODO: Back to Personal Data form
-                                }}                    
-                                displayType="secondary"
-                            >
-                                Previous
-                            </ClayButton>
-                        </div>
-
-                        <div className="btn-group-item">
-                            <ClayButton
-                                type="submit"
-                                displayType="primary"
-                            >
-                                Next
-                            </ClayButton>
-                        </div>
-                    </ClayButton.Group>
-                </div>                
-            </ClayForm>
-        </div>
+                    <div className="btn-group-item">
+                        <ClayButton
+                            type="submit"
+                            displayType="primary"
+                            onClick={() => {
+                                onSubmit();
+                                setCurrentStep(currentStep + 1)}
+                            }
+                        >
+                            Next
+                        </ClayButton>
+                    </div>
+                </ClayButton.Group>
+            </div>                
+        </ClayForm>
     )
 
 }
--- user-registration-form/src/components/PersonalData.js
+++ user-registration-form/src/components/PersonalData.js
@@ -6,7 +6,7 @@ import ClayPanel from '@clayui/panel';
 // Imports the @clayui/css package CSS
 import "@clayui/css/lib/css/atlas.css";
 
-const PersonalData = () => {
+const PersonalData = ({currentStep, setCurrentStep}) => {
 
     const [firstName, setFirstName] = useState([]);
     const [lastName, setLastName] = useState([]);
@@ -27,173 +27,167 @@ const PersonalData = () => {
     };
 
     return (
-        <div className="container col col-6">
-            <div className="text-center mt-3 mb-3">
-                <span>Step 1 of 3</span>
+        <ClayForm id="registration-personal-data" onSubmit={onSubmit}>
+
+            <ClayPanel displayTitle="Personal Data" displayType="unstyled">
+                <ClayPanel.Body>
+                    <ClayForm.Group>
+                        <label htmlFor="firstName">First Name</label>
+                        <ClayInput 
+                            id="firstName"
+                            name="firstName"
+                            placeholder="Insert your first name here"
+                            type="text"
+                            required
+                            onChange={(event) => {
+                                setFirstName(event.target.value);
+                            }}
+                        />
+                    </ClayForm.Group>
+
+                    <ClayForm.Group>
+                        <label htmlFor="lastName">Last Name</label>
+                        <ClayInput 
+                            id="lastName"
+                            name="lastName"
+                            placeholder="Insert your last name here"
+                            type="text"
+                            required
+                            onChange={(event) => {
+                                setLastName(event.target.value);
+                            }}
+                        />
+                    </ClayForm.Group>
+
+                    <ClayForm.Group>
+                        <label htmlFor="email">E-mail</label>
+                        <ClayInput 
+                            id="email"
+                            name="email"
+                            placeholder="Insert your e-mail address here"
+                            type="email"
+                            required
+                            onChange={(event) => {
+                                setEmail(event.target.value);
+                            }}
+                        />
+                    </ClayForm.Group>
+
+                    <ClayForm.Group>
+                        <label htmlFor="userName">Username</label>
+                        <ClayInput 
+                            id="userName"
+                            name="userName"
+                            placeholder="Insert your username here"
+                            type="text"
+                            required
+                            onChange={(event) => {
+                                setUserName(event.target.value);
+                            }}
+                        />
+                    </ClayForm.Group>
+
+                    <ClayForm.Group>
+                        <label htmlFor="genre">Genre</label>
+                        <ClaySelect 
+                            id="genre"
+                            name="genre"
+                            defaultValue={genre}
+                            onClick={(event) => {
+                                setGenre(event.target.value);                            
+                            }}
+
+                        >
+                            <ClaySelect.Option 
+                                key="1"
+                                label="Male"
+                                value="1"
+                            />
+                            <ClaySelect.Option 
+                                key="2"
+                                label="Female"
+                                value="2"
+                            />
+                            <ClaySelect.Option 
+                                key="3"
+                                label="Other"
+                                value="3"
+                            />
+                        </ClaySelect>
+                    </ClayForm.Group>
+
+                    <ClayForm.Group>
+                        <label htmlFor="birthday">Birthday</label>
+                        <ClayInput 
+                            id="birthday"
+                            name="birthday"
+                            placeholder="Insert your birthday here"
+                            type="date"
+                            required
+                            onChange={(event) => {
+                                setBirthday(event.target.value);
+                            }}
+                        />
+                    </ClayForm.Group>
+
+                    <ClayForm.Group>
+                        <label htmlFor="password">Password</label>
+                        <ClayInput 
+                            id="password"
+                            name="password"
+                            placeholder="Insert your password here"
+                            type="password"
+                            required
+                            onChange={(event) => {
+                                setPassword(event.target.value);
+                            }}
+                        />
+                    </ClayForm.Group>
+
+                    <ClayForm.Group>
+                        <label htmlFor="confirmPassword">Confirm your password</label>
+                        <ClayInput 
+                            id="confirmPassword"
+                            name="confirmPassword"
+                            placeholder="Confirm your password here"
+                            type="password"
+                            required
+                            onChange={(event) => {
+                                setConfirmPassword(event.target.value);
+                            }}
+                        />
+                    </ClayForm.Group>
+                </ClayPanel.Body>
+            </ClayPanel>
+
+            <div className="sheet-footer">
+                <ClayButton.Group>
+                    <div className="btn-group-item">
+                        <ClayButton
+                            displayType="secondary"
+                            onClick={() => {
+                                handleReset();
+                            }}
+                        >
+                            Cancel
+                        </ClayButton>
+                    </div>
+
+                    <div className="btn-group-item">
+                        <ClayButton
+                            type="submit"
+                            displayType="primary"
+                            onClick={() => {
+                                onSubmit();
+                                setCurrentStep(currentStep + 1)}
+                            }
+                        >
+                            Next
+                        </ClayButton>
+                    </div>
+                </ClayButton.Group>
             </div>
-
-            <h3 className="text-center mb-3">
-                Let's register your account!
-            </h3>
-
-            <ClayForm id="registration-personal-data" onSubmit={onSubmit}>
-
-                <ClayPanel displayTitle="Personal Data" displayType="unstyled">
-                    <ClayPanel.Body>
-                        <ClayForm.Group>
-                            <label htmlFor="firstName">First Name</label>
-                            <ClayInput 
-                                id="firstName"
-                                name="firstName"
-                                placeholder="Insert your first name here"
-                                type="text"
-                                required
-                                onChange={(event) => {
-                                    setFirstName(event.target.value);
-                                }}
-                            />
-                        </ClayForm.Group>
-
-                        <ClayForm.Group>
-                            <label htmlFor="lastName">Last Name</label>
-                            <ClayInput 
-                                id="lastName"
-                                name="lastName"
-                                placeholder="Insert your last name here"
-                                type="text"
-                                required
-                                onChange={(event) => {
-                                    setLastName(event.target.value);
-                                }}
-                            />
-                        </ClayForm.Group>
-
-                        <ClayForm.Group>
-                            <label htmlFor="email">E-mail</label>
-                            <ClayInput 
-                                id="email"
-                                name="email"
-                                placeholder="Insert your e-mail address here"
-                                type="email"
-                                required
-                                onChange={(event) => {
-                                    setEmail(event.target.value);
-                                }}
-                            />
-                        </ClayForm.Group>
-
-                        <ClayForm.Group>
-                            <label htmlFor="userName">Username</label>
-                            <ClayInput 
-                                id="userName"
-                                name="userName"
-                                placeholder="Insert your username here"
-                                type="text"
-                                required
-                                onChange={(event) => {
-                                    setUserName(event.target.value);
-                                }}
-                            />
-                        </ClayForm.Group>
-
-                        <ClayForm.Group>
-                            <label htmlFor="genre">Genre</label>
-                            <ClaySelect 
-                                id="genre"
-                                name="genre"
-                                defaultValue={genre}
-                                onClick={(event) => {
-                                    setGenre(event.target.value);                            
-                                }}
-
-                            >
-                                <ClaySelect.Option 
-                                    key="1"
-                                    label="Male"
-                                    value="1"
-                                />
-                                <ClaySelect.Option 
-                                    key="2"
-                                    label="Female"
-                                    value="2"
-                                />
-                                <ClaySelect.Option 
-                                    key="3"
-                                    label="Other"
-                                    value="3"
-                                />
-                            </ClaySelect>
-                        </ClayForm.Group>
-
-                        <ClayForm.Group>
-                            <label htmlFor="birthday">Birthday</label>
-                            <ClayInput 
-                                id="birthday"
-                                name="birthday"
-                                placeholder="Insert your birthday here"
-                                type="date"
-                                required
-                                onChange={(event) => {
-                                    setBirthday(event.target.value);
-                                }}
-                            />
-                        </ClayForm.Group>
-
-                        <ClayForm.Group>
-                            <label htmlFor="password">Password</label>
-                            <ClayInput 
-                                id="password"
-                                name="password"
-                                placeholder="Insert your password here"
-                                type="password"
-                                required
-                                onChange={(event) => {
-                                    setPassword(event.target.value);
-                                }}
-                            />
-                        </ClayForm.Group>
-
-                        <ClayForm.Group>
-                            <label htmlFor="confirmPassword">Confirm your password</label>
-                            <ClayInput 
-                                id="confirmPassword"
-                                name="confirmPassword"
-                                placeholder="Confirm your password here"
-                                type="password"
-                                required
-                                onChange={(event) => {
-                                    setConfirmPassword(event.target.value);
-                                }}
-                            />
-                        </ClayForm.Group>
-                    </ClayPanel.Body>
-                </ClayPanel>
-
-                <div className="sheet-footer">
-                    <ClayButton.Group>
-                        <div className="btn-group-item">
-                            <ClayButton
-                                onClick={() => {
-                                    handleReset();
-                                }}                    
-                                displayType="secondary"
-                            >
-                                Cancel
-                            </ClayButton>
-                        </div>
-
-                        <div className="btn-group-item">
-                            <ClayButton
-                                type="submit"
-                                displayType="primary"
-                            >
-                                Next
-                            </ClayButton>
-                        </div>
-                    </ClayButton.Group>
-                </div>
-            </ClayForm>
-        </div>       
+        </ClayForm>
     )
     
 }
