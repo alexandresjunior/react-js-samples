78ef15cd085758a593e9478d42e59e35f411e238
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2022-03-03 14:15:21 -0300

    Map products from json server

--- ecommerce-app/src/components/Homepage.js
+++ ecommerce-app/src/components/Homepage.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { search } from "../api/index";
 
 const Homepage = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        search("/products", setProducts);
+    }, []);
 
     return (
         <>
@@ -32,21 +38,24 @@ const Homepage = () => {
             <section className="py-5">
                 <div className="container px-4 px-lg-5 mt-5">
                     <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                        <div className="col mb-5">
-                            <div className="card h-100">
-                                <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
-                                     alt="..."/>
-                                <div className="card-body p-4">
-                                    <div className="text-center">
-                                        <h5 className="fw-bolder">Fancy Product</h5>
-                                        $40.00 - $80.00
+                        {products.map((product) => (
+                            <div className="col mb-5">
+                                <div className="card h-100">
+                                    <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+                                         alt="..."/>
+                                    <div className="card-body p-4">
+                                        <div className="text-center">
+                                            <h5 className="fw-bolder">{product.name}</h5>
+                                            <h6 className="fw-bolder">{product.category}</h6>
+                                            $40.00 - $80.00
+                                        </div>
+                                    </div>
+                                    <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                        <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
                                     </div>
                                 </div>
-                                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                    <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
-                                </div>
                             </div>
-                        </div>
+                        ))}
                     </div>
                 </div>
             </section>
