764537bfc3ec09db369af75751c95b8898126ae1
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2022-03-03 14:53:10 -0300

    Refactor homepage code; split into smaller components

---
+++ ecommerce-app/src/components/homepage/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="bg-dark py-5">
+            <div className="container px-4 px-lg-5 my-5">
+                <div className="text-center text-white">
+                    <h1 className="display-4 fw-bolder">Don't bake, buy a cake!</h1>
+                    <p className="lead fw-normal text-white-50 mb-0">Also, get sweets and cookies!</p>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
---
+++ ecommerce-app/src/components/homepage/Section.js
@@ -0,0 +1,39 @@
+import React, {useEffect, useState} from "react";
+import { search } from "../../api/index";
+
+const Section = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        search("/products", setProducts);
+    }, []);
+
+    return (
+        <section className="py-5">
+            <div className="container px-4 px-lg-5 mt-5">
+                <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                    {products.map((product) => (
+                        <div className="col mb-5">
+                            <div className="card h-100">
+                                <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+                                     alt="..."/>
+                                <div className="card-body p-4">
+                                    <div className="text-center">
+                                        <h5 className="fw-bolder">{product.name}</h5>
+                                        <h6 className="fw-bolder">{product.category}</h6>
+                                        $40.00 - $80.00
+                                    </div>
+                                </div>
+                                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                    <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Section;
